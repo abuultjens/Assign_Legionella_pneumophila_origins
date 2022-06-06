139d7d2cc04da93c83f5926d99ca582958e14a1b
Andrew Buultjens <buultjensa@gmail.com>
2022-06-06 21:12:46 +1000

    Update train_report-maker.sh

--- train_report-maker.sh
+++ train_report-maker.sh
@@ -18,17 +18,18 @@ cut -f 9 -d ',' ${CONFIG} > ${RAND}_prefix.txt
 for TAXA in $(cat ${RAND}_prefix.txt); do
 
 	PREFIX=${TAXA}
-
-        TN_val=`head -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 1 -d ','` 
-        FP_val=`head -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 2 -d ','`  
-        FN_val=`tail -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 1 -d ','`  
-        TP_val=`tail -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 2 -d ','`
-	precision_val=`cat ${PREFIX}_precision_score_val.csv`
-	recall_val=`cat ${PREFIX}_recall_score_val.csv`
-	f1_val=`cat ${PREFIX}_f1_score_val.csv`
-	AUC_val=`cat ${PREFIX}_AUC_val.csv`
 	
 	if ls ${PREFIX}_confusion_matrix_val.csv 1> /dev/null 2>&1; then
+
+        	TN_val=`head -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 1 -d ','` 
+ 	       FP_val=`head -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 2 -d ','`  
+	        FN_val=`tail -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 1 -d ','`  
+	        TP_val=`tail -1 ${PREFIX}_confusion_matrix_val.csv | cut -f 2 -d ','`
+		precision_val=`cat ${PREFIX}_precision_score_val.csv`
+		recall_val=`cat ${PREFIX}_recall_score_val.csv`
+		f1_val=`cat ${PREFIX}_f1_score_val.csv`
+		AUC_val=`cat ${PREFIX}_AUC_val.csv`
+	
 		echo "${PREFIX},${TP_val},${TN_val},${FP_val},${FN_val},${precision_val},${recall_val},${f1_val},${AUC_val}" >> TRAIN_report.csv
 	fi
 
