c4c7c86bd84ddf0cde594d71933cd65b38368abb
Andrew Buultjens <buultjensa@gmail.com>
2023-03-16 12:49:12 +1100

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -22,36 +22,4 @@ np.savetxt("%s_confusion_matrix_dist.csv" % PREFIX, CM, delimiter=",", fmt="%s")
 F1 = f1_score(df['ORIGINAL_CLASS'], df['PRED_CLASS'])
 F1 = np.array([F1])
 np.savetxt("%s_f1_score_dist.csv" % PREFIX, F1, delimiter=",", fmt="%s")
-
-### calculate rank
-# make arrays
-percents = []
-percent_obtained = []
-total = []
-obtained = []
-bit = []
-
-df_sorted = df.sort_values('VALUE',ascending=False)
-
-for d in range(1, 1001):
-        percents.append((d/1000)*100)
-
-        TOTAL = df['ORIGINAL_CLASS'].value_counts().get(1, 0)
-        total.append(TOTAL)
-        
-        OBTAINED = df_sorted.head(n=(round((int(df['ORIGINAL_CLASS'].count()))*(d/1000))))['ORIGINAL_CLASS'].value_counts().get(1, 0)
-        obtained.append(OBTAINED)
-        
-        percent_obtained.append((OBTAINED/TOTAL)*100)
-        
-        if ((OBTAINED/TOTAL)*100) == 100:
-                BIT="ALL"
-        if ((OBTAINED/TOTAL)*100) < 100:
-                BIT="NA"
-        bit.append(BIT)
-
-RANK = np.vstack([percents, percent_obtained, total, obtained, bit]).T
-
-np.savetxt("%s_RANK_dist.csv" % PREFIX, RANK, delimiter=",", fmt="%s") 
-
         
