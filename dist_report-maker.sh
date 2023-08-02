caf52017301ea0bf9b66909cc5af854941b29b66
Andrew Buultjens <buultjensa@gmail.com>
2023-08-02 14:03:15 +1000

    Update dist_report-maker.sh

--- dist_report-maker.sh
+++ dist_report-maker.sh
@@ -16,8 +16,6 @@ for OB in $(cat $1); do
 	TP_dist=`tail -1 ${NAME}_confusion_matrix_dist.csv | cut -f 2 -d ','`  
         f1_dist=`cat ${NAME}_f1_score_dist.csv`
 	auc_dist=`cat ${NAME}_auc_dist.csv`
-	rank_dist=`grep "ALL" ${NAME}_RANK_dist.csv | head -1 | cut -f 1 -d ','`
-
 
 	echo "${NAME},${TP_dist},${TN_dist},${FP_dist},${FN_dist},${f1_dist},${auc_dist},${rank_dist}" >> ${PREFIX}_DIST_report.csv
 
